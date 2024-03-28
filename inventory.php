00c1015b5f11bfade90c7b17914cf24970a26076
Zenji Yangco <zenjiyangco@gmail.com>
2024-03-29 01:00:41 +0800

    Inventory

--- inventory.php
+++ inventory.php
@@ -16,15 +16,13 @@ if (strlen($_SESSION['employee_id']) === 0) {
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <table class="table table-bordered">
+                        <table class="inv-color-table table-bordered">
                             <thead>
                                 <tr>
                                     <th class="align-middle text-center bg-info" style="width: 10%;">Category</th>
-                                    <th class="align-middle text-center bg-info" style="width: 10%;"> Porduct</th>
                                     <th class="align-middle text-center bg-info" style="width: 10%;"> Brand name </th>
                                     <th class="align-middle text-center bg-info" style="width: 10%;"> Quantity Stock</th>
                                     <th class="align-middle text-center bg-info" style="width: 10%;"> Storage Location</th>
-                                    <th class="align-middle text-center bg-info" style="width: 10%;"> Date Added</th>
                                     <th class="align-middle text-center bg-info" style="width: 10%;"> Expiration Date</th>
                                     <th class="align-middle text-center bg-info" style="width: 10%;"> Showroom Quantity Stock</th>
                                     <th class="align-middle text-center bg-info" style="width: 10%;"> Showroom Location</th>
@@ -35,27 +33,14 @@ if (strlen($_SESSION['employee_id']) === 0) {
                                 <tr>
                                     <td class='align-middle text-center'>Paracetamol</td>
                                     <td class='align-middle text-center'>Biogesic</td>
-                                    <td class='align-middle text-center'>Ritemed</td>
                                     <td class='align-middle text-center'>1000</td>
                                     <td class='align-middle text-center'>IS 1</td>
-                                    <td class='align-middle text-center'>Mar 13, 2024, 5:50pm</td>
                                     <td class='align-middle text-center'>Feb 20, 2029</td>
                                     <td class='align-middle text-center'>100</td>
                                     <td class='align-middle text-center'>SR 1</td>
                                     <td class='align-middle text-center'>100</td>
                                 </tr>
-                                <tr>
-                                    <td class='align-middle text-center'>Loperamide</td>
-                                    <td class='align-middle text-center'>Diatabs</td>
-                                    <td class='align-middle text-center'>UNILAB</td>
-                                    <td class='align-middle text-center'>1000</td>
-                                    <td class='align-middle text-center'>IS 2</td>
-                                    <td class='align-middle text-center'>Mar 18, 2024, 5:50pm</td>
-                                    <td class='align-middle text-center'>Dec 10, 2031</td>
-                                    <td class='align-middle text-center'>100</td>
-                                    <td class='align-middle text-center'>SR 2</td>
-                                    <td class='align-middle text-center'>100</td>
-                                </tr>
+
                             </tbody>
                         </table>
                     </div>
--- inventory_logs.php
+++ inventory_logs.php
@@ -14,104 +14,50 @@ if (strlen($_SESSION['employee_id']) === 0) {
         </div>
         <div class="row mb-2">
             <div class="col-md-6 d-flex">
-                <input type="text" id="searchInput" class=" form-control" style="width: 200px;" placeholder="Search">
+                <input type="text" id="searchInput" class=" form-control" style="width: 200px; height: 30px;" placeholder="Search">
             </div>
             <div class="col-md-6 d-flex justify-content-end">
-                <button class="btn btn-primary">Export Logs</button>
+                <button class="btn btn-primary" style="height: 40px;">Export Logs</button>
             </div>
         </div>
-        <table class="table table-bordered">
+        <table class="inv-color-table table-bordered">
             <thead>
                 <tr>
                     <th class="bg-info">Date</th>
                     <th class="bg-info">Product</th>
+                    <th class="bg-info">Employee</th>
                     <th class="bg-info">Quantity</th>
-                    <th class="bg-info">Beginning -> Ending Stock On Hand</th>
+                    <th class="bg-info">Stock after</th>
                     <th class="bg-info">Transaction</th>
-                    <th class="bg-info">User</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>Mar 13, 2024, 5:50pm</td>
                     <td>Biogesic</td>
+                    <td>Zenji</td>
                     <td>+1000</td>
                     <td>0 -> 1000</td>
                     <td>Stock Order</td>
-                    <td>Zenji</td>
                 </tr>
                 <tr>
                     <td>Mar 14, 2024, 6:50pm</td>
                     <td>Biogesic</td>
+                    <td>Loi</td>
                     <td>-10</td>
                     <td>1000 -> 990</td>
                     <td>Purchase Order</td>
-                    <td>Loi</td>
                 </tr>
                 <tr>
                     <td>Mar 15, 2024, 7:50pm</td>
                     <td>Biogesic</td>
+                    <td>Loi</td>
                     <td>+10</td>
                     <td>990 -> 1000</td>
                     <td>Purchase Return</td>
-                    <td>Loi</td>
+
                 </tr>
-                <tr>
-                    <td>Mar 15, 2024, 7:50pm</td>
-                    <td>Biogesic</td>
-                    <td>+10</td>
-                    <td>990 -> 1000</td>
-                    <td>Purchase Return</td>
-                    <td>Loi</td>
-                </tr>
-                <tr>
-                    <td>Mar 15, 2024, 7:50pm</td>
-                    <td>Biogesic</td>
-                    <td>+10</td>
-                    <td>990 -> 1000</td>
-                    <td>Purchase Return</td>
-                    <td>Loi</td>
-                </tr>
-                <tr>
-                    <td>Mar 15, 2024, 7:50pm</td>
-                    <td>Biogesic</td>
-                    <td>+10</td>
-                    <td>990 -> 1000</td>
-                    <td>Purchase Return</td>
-                    <td>Loi</td>
-                </tr>
-                <tr>
-                    <td>Mar 15, 2024, 7:50pm</td>
-                    <td>Biogesic</td>
-                    <td>+10</td>
-                    <td>990 -> 1000</td>
-                    <td>Purchase Return</td>
-                    <td>Loi</td>
-                </tr>
-                <tr>
-                    <td>Mar 15, 2024, 7:50pm</td>
-                    <td>Biogesic</td>
-                    <td>+10</td>
-                    <td>990 -> 1000</td>
-                    <td>Purchase Return</td>
-                    <td>Loi</td>
-                </tr>
-                <tr>
-                    <td>Mar 15, 2024, 7:50pm</td>
-                    <td>Biogesic</td>
-                    <td>+10</td>
-                    <td>990 -> 1000</td>
-                    <td>Purchase Return</td>
-                    <td>Loi</td>
-                </tr>
-                <tr>
-                    <td>Mar 15, 2024, 7:50pm</td>
-                    <td>Biogesic</td>
-                    <td>+10</td>
-                    <td>990 -> 1000</td>
-                    <td>Purchase Return</td>
-                    <td>Loi</td>
-                </tr>
+
             </tbody>
         </table>
     </div>
